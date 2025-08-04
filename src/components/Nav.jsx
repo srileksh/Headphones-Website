8140d0cd57c6cc9d295c69e509de04bb1398bdc8
SreelakshmiJ <sreelakshmij45@gmail.com>
2025-08-04 14:29:40 +0530

    modified Sec1 and Sec5

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
         <img className='w-[30px] h-[30px] sm:w-[50px] sm:h-[50px]' src="Waveform.png" alt="" />
         <h2 className='text-[#52483E] text-[15px] sm:text-2xl font-semibold'>Resonance</h2>
         </div>
-        <div className='flex gap-7 sm:gap-10 md:gap-15 lg:gap-20'>
+        <div className='flex gap-7 sm:gap-10 md:gap-15 lg:gap-20 items-center'>
         <Link to="/"><p className='text-[#52483E] text-[12px] sm:text-[20px]'>Home</p></Link>   
              <Link to="techspecs"><p className='text-[#52483E] text-[12px] sm:text-[20px]'>Tech Specs</p></Link>
         <Link to="compare"><button className='border-1 px-1.5 py-0 sm:px-3 sm:py-1 rounded-2xl text-white bg-[#52483E] text-[12px] sm:text-[20px]'>Compare</button></Link>
